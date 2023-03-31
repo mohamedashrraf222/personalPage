2c5a6396e37e9e3431732fc523c6da6fbe085109
Mohamed Ashraf <ma1913920@gmail.com>
2023-03-31 14:24:59 +0200

    Updates

---
+++ projects.js
@@ -0,0 +1,76 @@
+let projects = [
+  {
+    name: "tenzies-game",
+    descreption:
+      "This is a tenzies-game that I have created along with scrimba's React course. In this project I used React components and states to update the page with the user interactions",
+    pic: "/images/tenzies-game.jpg",
+    github: "https://github.com/mohamedashrraf222/tenzies-game.git",
+    key: 1,
+    demo: "https://mohamedashrraf222.github.io/tenzies-game/",
+  },
+  {
+    name: "My Reads App",
+    descreption:
+      "This project I created with React from a starter code was provided in Udacity's React course that contained only CSS and HTML elements, it was without any functionality and I have created all its functions as a graduation project. I use an API to add or update or remove books from the main page.",
+    pic: "/images/myReads.jpg",
+    github: "https://github.com/mohamedashrraf222/myReadsApp.git",
+    key: 2,
+    demo: "https://mohamedashrraf222.github.io/myReadsApp/",
+  },
+  {
+    name: "weather Journal App",
+    descreption:
+      "I have created for this project a backend server using nodeJS, you can check the server files and code on GitHub. I am using an API to get data according to user input with jQuery, JavaScript and promis functions, then fetch and filter data returned to get the weather. ",
+    pic: "/images/weather.jpg",
+    github: "https://github.com/mohamedashrraf222/Weather-Journal-App.git",
+    key: 3,
+    demo: "https://codepen.io/mohamedashrraf222/pen/oNyWqap",
+  },
+  {
+    name: "meme genrator",
+    descreption:
+      "This project I created along with React Scrimba course. It's using an API to get the best 100 memes pictures then randomly choose one of them to put your two inputs on them to create funny memes.",
+    pic: "/images/memeGen.jpg",
+    github: "https://github.com/mohamedashrraf222/memeGenratorScrimba.git",
+    key: 4,
+    demo: "https://mohamedashrraf222.github.io/memeGenratorScrimba/",
+  },
+  {
+    name: "personal business card",
+    descreption:
+      "This project is simple, I have created it for myself and as a training for creating a static react page",
+    pic: "/images/personalCard.jpg",
+    github: "https://github.com/mohamedashrraf222/business-card.git",
+    key: 5,
+    demo: "https://mohamedashrraf222.github.io/business-card/",
+  },
+  {
+    name: "React Facts",
+    descreption:
+      "This is a very simple project that I have created along with Scrimba React course as a training for creating a static react page",
+    pic: "/images/react facts.jpg",
+    github: "https://github.com/mohamedashrraf222/ReactFactsScrimba.git",
+    key: 6,
+    demo: "https://mohamedashrraf222.github.io/ReactFactsScrimba/",
+  },
+  {
+    name: "Travel Journal",
+    descreption:
+      "This is a very simple project that I have created as a training for creating a static react page",
+    pic: "/images/travelJornal.jpg",
+    github: "https://github.com/mohamedashrraf222/travel-journal.git",
+    key: 7,
+    demo: "https://mohamedashrraf222.github.io/travel-journal/",
+  },
+  {
+    name: "Mutiple page",
+    descreption:
+      "This is a very simple static page that's created with only css and html that I created when I was learning html and css with Freecodecamp.",
+    pic: "/images/myPage.jpg",
+    github: "",
+    key: 8,
+    demo: "https://codepen.io/mohamedashrraf222/pen/RwaYaYy?editors=1100",
+  },
+];
+
+export default projects;
