c3dbf73b91c96a7165dc05a8426f0da8e5fa61d3
Mohamed Ashraf <ma1913920@gmail.com>
2023-04-10 02:23:24 +0200

    Updates

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@ let projects = [
     name: "tenzies-game",
     descreption:
       "This is a tenzies-game that I have created along with scrimba's React course. In this project I used React components and states to update the page with the user interactions",
-    pic: "/personalPage/images/tenzies-game.jpg",
+    pic: "/images/tenzies-game.jpg",
     github: "https://github.com/mohamedashrraf222/tenzies-game.git",
     key: 1,
     demo: "https://mohamedashrraf222.github.io/tenzies-game/",
@@ -12,7 +12,7 @@ let projects = [
     name: "My Reads App",
     descreption:
       "This project I created with React from a starter code was provided in Udacity's React course that contained only CSS and HTML elements, it was without any functionality and I have created all its functions as a graduation project. I use an API to add or update or remove books from the main page.",
-    pic: "/personalPage/images/myReads.jpg",
+    pic: "/images/myReads.jpg",
     github: "https://github.com/mohamedashrraf222/myReadsApp.git",
     key: 2,
     demo: "https://mohamedashrraf222.github.io/myReadsApp/",
@@ -21,7 +21,7 @@ let projects = [
     name: "weather Journal App",
     descreption:
       "I have created for this project a backend server using nodeJS, you can check the server files and code on GitHub. I am using an API to get data according to user input with jQuery, JavaScript and promis functions, then fetch and filter data returned to get the weather. ",
-    pic: "/personalPage/images/weather.jpg",
+    pic: "/images/weather.jpg",
     github: "https://github.com/mohamedashrraf222/Weather-Journal-App.git",
     key: 3,
     demo: "https://codepen.io/mohamedashrraf222/pen/oNyWqap",
@@ -30,7 +30,7 @@ let projects = [
     name: "meme genrator",
     descreption:
       "This project I created along with React Scrimba course. It's using an API to get the best 100 memes pictures then randomly choose one of them to put your two inputs on them to create funny memes.",
-    pic: "/personalPage/images/memeGen.jpg",
+    pic: "/images/memeGen.jpg",
     github: "https://github.com/mohamedashrraf222/memeGenratorScrimba.git",
     key: 4,
     demo: "https://mohamedashrraf222.github.io/memeGenratorScrimba/",
@@ -39,7 +39,7 @@ let projects = [
     name: "personal business card",
     descreption:
       "This project is simple, I have created it for myself and as a training for creating a static react page",
-    pic: "/personalPage/images/personalCard.jpg",
+    pic: "/images/personalCard.jpg",
     github: "https://github.com/mohamedashrraf222/business-card.git",
     key: 5,
     demo: "https://mohamedashrraf222.github.io/business-card/",
@@ -48,7 +48,7 @@ let projects = [
     name: "React Facts",
     descreption:
       "This is a very simple project that I have created along with Scrimba React course as a training for creating a static react page",
-    pic: "/personalPage/images/react facts.jpg",
+    pic: "/images/react facts.jpg",
     github: "https://github.com/mohamedashrraf222/ReactFactsScrimba.git",
     key: 6,
     demo: "https://mohamedashrraf222.github.io/ReactFactsScrimba/",
@@ -57,7 +57,7 @@ let projects = [
     name: "Travel Journal",
     descreption:
       "This is a very simple project that I have created as a training for creating a static react page",
-    pic: "/personalPage/images/travelJornal.jpg",
+    pic: "/images/travelJornal.jpg",
     github: "https://github.com/mohamedashrraf222/travel-journal.git",
     key: 7,
     demo: "https://mohamedashrraf222.github.io/travel-journal/",
@@ -66,7 +66,7 @@ let projects = [
     name: "Mutiple page",
     descreption:
       "This is a very simple static page that's created with only css and html that I created when I was learning html and css with Freecodecamp.",
-    pic: "/personalPage/images/myPage.jpg",
+    pic: "/images/myPage.jpg",
     github: "",
     key: 8,
     demo: "https://codepen.io/mohamedashrraf222/pen/RwaYaYy?editors=1100",
